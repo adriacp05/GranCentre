3fe30d6a47a0a7aa82337e9963fff0cd6a72dc85
adriacp05 <adriacp05@hotmail.es>
2018-10-22 09:49:24 +0200

    l

--- app/src/main/java/com/example/adria/grancentre/restaurants.java
+++ app/src/main/java/com/example/adria/grancentre/restaurants.java
@@ -2,12 +2,25 @@ package com.example.adria.grancentre;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class restaurants extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerrestaurants);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.restaurantes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        ;
+        spinner.setAdapter(adapter);
+
     }
 }
